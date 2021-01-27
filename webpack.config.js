f58a4ac5bb23a4c7ab42ed1e1af2315104c9a072
ricardo <ramn21@gmail.com>
2021-01-27 15:50:56 +0100

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const { mainModule } = require('process');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
