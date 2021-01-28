afdb423d17e9b872e2fbc0b0272d86e6724c5f2f
ricardo <ramn21@gmail.com>
2021-01-28 01:08:40 +0100

    add svg in navbar.js add styles  navbar

--- src/assets/navbar.js
+++ src/assets/navbar.js
@@ -1,5 +1,11 @@
+import { createSVG } from './createSvg'
+
 class CreateNavbar {
   creation(){
+    const menuText = "Menu";
+    const contactText = "Contact"
+    let svg1 = '';
+    let svg2 = '';
     const content = document.body
     const header = document.createElement('header');
     const container = document.createElement('div');
@@ -8,16 +14,19 @@ class CreateNavbar {
     const menu = document.createElement('li');
     const contact = document.createElement('li');
     logo.textContent = 'Balkan Pasion'; 
-    menu.textContent = 'Our Menu';
-    contact.textContent = "Contact Us";
+    svg1 = createSVG(menuText)
+    svg2 = createSVG(contactText)
     logo.id = 'home'
-    menu.id = 'menu';
-    contact.id = 'contact';
     container.id = 'contet'
+    svg1.classList.add('tab');
+    svg2.classList.add('tab2')
+    menu.append(svg1)
+    contact.append(svg2)
     ul.append(menu);
     ul.append(contact);
     container.append(logo);
     container.append(ul);
+    logo.classList.add('logo')
     container.classList.add('nav');
     header.append(container);
     content.prepend(header);
