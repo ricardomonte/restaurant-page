26599251da97d537a13780a88e2adbbcfc630c4f
ricardo <ramn21@gmail.com>
2021-01-29 02:14:34 +0100

    add loaders and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 const path = require('path');
 // const { mainModule } = require('process');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: '!!html-loader!./src/template.html',
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -15,6 +25,18 @@ module.exports = {
       {
         test: /\.scss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|cur)$/i,
+        use: [ 
+          { 
+            loader: 'file-loader',
+          }
+        ]
+      },
+      {
+        test: /\.html$/i,
+        use: [ 'file-loader', 'html-loader' ]
       },
     ],
   },
